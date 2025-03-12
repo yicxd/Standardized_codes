da315830fc9bdcb5d64b708ec91056eb12e3ed43
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 00:42:17 +0800

    asking for 10 inputs

---
+++ no_duplicates_in_10nums.py
@@ -0,0 +1,13 @@
+count = 0
+fcount = 10
+
+while True:
+    try:
+        num = int(input("Enter a whole number: "))
+        count = count + 1
+        if count != fcount: #stops after asking the 10th number
+            continue
+        else:
+            break
+    except ValueError:
+        print("please enter a whole number")
