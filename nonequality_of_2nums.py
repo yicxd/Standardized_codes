e0beda5831b78455bfab059d0ca91b517f91bcff
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 00:01:01 +0800

    already standardized

---
+++ nonequality_of_2nums.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        print("Enter two whole numbers")
+        x1 = int(input("enter first number:"))
+        x2 = int(input("enter second number:"))
+        break
+    except ValueError:
+        print("try entering whole numbers")
+        continue
+
+if x1 != x2:
+    print("not equal")
---
+++ smaller_of_2nums.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        print("Enter two whole numbers")
+        x1 = int(input("enter first number:"))
+        x2 = int(input("enter second number:"))
+        break
+    except ValueError:
+        print("try entering whole numbers")
+        continue
+
+if x1 < x2:
+    print(x1)
+else:
+    print(x2)
