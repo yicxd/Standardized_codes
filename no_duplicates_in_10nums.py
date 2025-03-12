ccf5eafcf7688d64ac4b3e687dd91f265a91e597
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 01:43:32 +0800

    added another list for dupes and nondupes, and a for loop to check for dupes

--- no_duplicates_in_10nums.py
+++ no_duplicates_in_10nums.py
@@ -1,9 +1,15 @@
-list = []
+list = [] #list of all numbers
+dupes = [] #list of all duplicates
+nondupe = set() #set stores unique numbers only
 
-
-for i in range(10):
+for i in range(10): #ask for 10 numbers
     num = int(input("Enter a whole number: "))
-    list.append(num)
+    list.append(num) #puts the numbers in the list
 
+for num in list:
+    if num in nondupe:
+        dupes.append(num)
+    else:
+        nondupe.add(num)
 
-print(list)
+print(nondupe)
