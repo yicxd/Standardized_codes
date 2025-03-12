7023872d5493626d0d859aae3ecab438189c0464
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 01:10:24 +0800

    used for loop instead

--- no_duplicates_in_10nums.py
+++ no_duplicates_in_10nums.py
@@ -1,17 +1,9 @@
-count = 0
-fcount = 10
 list = []
 
-while True:
-    try:
-        num = int(input("Enter a whole number: "))
-        count = count + 1
-        list.append(num)
-        if count != fcount: #stops after asking the 10th number
-            continue
-        else:
-            break
-    except ValueError:
-        print("please enter a whole number")
+
+for i in range(10):
+    num = int(input("Enter a whole number: "))
+    list.append(num)
+
 
 print(list)
