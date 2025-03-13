59cf9a5907a5b23d8ee3881666ef69aa680131e3
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 21:11:13 +0800

    added a set and number checker

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,9 +1,15 @@
 list = []
+og = set()
 
 while True: #this loop will ask the user until value is invalid
     try:
         num = int(input("Enter a whole number: "))
         list.append(num)
+        if num not in og: #if number is new adds it to the og set
+            print("unique")
+            og.add(num)
+        else: #if number is found in og set its a duplicate
+            print("duplicate")
         continue
     except ValueError: 
         break
