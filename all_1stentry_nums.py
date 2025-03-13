5a9913a1a3d733642d98de4fac513155830e46b7
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 19:25:31 +0800

    added a set and added the numbers to the set

--- all_1stentry_nums.py
+++ all_1stentry_nums.py
@@ -1,5 +1,11 @@
 list = [] #list of all numbers
+n1 = set() #set only stores unique numbers
 
 for i in range(10): #ask for 10 numbers
     num = int(input("Enter a whole number: "))
     list.append(num) #puts the numbers in the list
+
+for num in list:
+    n1.add(num)
+
+print(n1)
