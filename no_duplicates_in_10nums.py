4f0b6c1761f9f3116af4bb585f1dc946cd8dfa37
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 18:54:42 +0800

    used count() instead

--- no_duplicates_in_10nums.py
+++ no_duplicates_in_10nums.py
@@ -1,15 +1,12 @@
 list = [] #list of all numbers
-dupes = [] #list of all duplicates
-nondupe = set() #set stores unique numbers only
+nondupe = [] #set stores unique numbers only
 
 for i in range(10): #ask for 10 numbers
     num = int(input("Enter a whole number: "))
     list.append(num) #puts the numbers in the list
 
 for num in list:
-    if num in nondupe:
-        dupes.append(num)
-    else:
-        nondupe.add(num)
+    if list.count(num) == 1: #count checks how many times a number appears
+        nondupe.append(num)
 
 print(nondupe)
