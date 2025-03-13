ca5d5cd1e5fba311972b723798e709327db7b50c
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-13 20:29:56 +0800

    the input will be added to a list

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,6 +1,9 @@
-while True:
+list = []
+
+while True: #this loop will ask the user until value is invalid
     try:
         num = int(input("Enter a whole number: "))
+        list.append(num)
         continue
-    except ValueError:
+    except ValueError: 
         break
